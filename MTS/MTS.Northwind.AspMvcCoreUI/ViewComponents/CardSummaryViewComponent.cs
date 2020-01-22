b5df156080f129fb03b4b78dc23c2ccf85384e6d
mtsoydan <mustafataha.soydan@gmail.com>
2020-01-23 01:06:03 +0300

    CardSummaryViewCompents Tamamlandı
    Sessiondaki Card verileri dropdownMenu ye aktarıldı

---
+++ MTS/MTS.Northwind.AspMvcCoreUI/ViewComponents/CardSummaryViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using MTS.Northwind.AspMvcCoreUI.Models;
+using MTS.Northwind.AspMvcCoreUI.Services;
+using MTS.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTS.Northwind.AspMvcCoreUI.ViewComponents
+{
+    public class CardSummaryViewComponent: ViewComponent
+    {
+        ICartSessionService _cartSessionService;
+        public CardSummaryViewComponent(ICartSessionService cartSessionService)
+
+        {
+            _cartSessionService = cartSessionService;
+        }
+         
+        public ViewViewComponentResult Invoke()
+        {
+
+            var model = new CartSummaryViewModel
+            {
+                Cart = _cartSessionService.GetCart()
+            };
+            return View(model);
+
+        }
+
+
+    }
+}
+
