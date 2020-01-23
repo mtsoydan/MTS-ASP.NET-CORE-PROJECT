62e777e9b37eaecdd5440d472d696285ac746a12
mtsoydan <mustafataha.soydan@gmail.com>
2020-01-24 01:17:50 +0300

    Admin Paneli Ürün Listelenmesi Gerçekleştirildi

---
+++ MTS/MTS.Northwind.AspMvcCoreUI/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MTS.Northwind.AspMvcCoreUI.Models;
+using MTS.Northwind.Business.Abstract;
+
+namespace MTS.Northwind.AspMvcCoreUI.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        IProductService _productService;
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public ActionResult Index()
+        {
+            var ProductListViewModel = new ProductListViewModel
+            {
+                Products = _productService.GetAll()
+            };
+
+            return View(ProductListViewModel);
+
+        
+        }
+    }
+}
