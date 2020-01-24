4ebff532fa487137053fcfb2b5235f0212782ade
mtsoydan <mustafataha.soydan@gmail.com>
2020-01-24 15:52:37 +0300

    Product Ekleme için gerekli view ve metodlar olusturuldu

--- MTS/MTS.Northwind.AspMvcCoreUI/Controllers/AdminController.cs
+++ MTS/MTS.Northwind.AspMvcCoreUI/Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MTS.Northwind.AspMvcCoreUI.Models;
 using MTS.Northwind.Business.Abstract;
+using MTS.Northwind.Entities.Concrete;
 
 namespace MTS.Northwind.AspMvcCoreUI.Controllers
 {
@@ -18,6 +19,7 @@ namespace MTS.Northwind.AspMvcCoreUI.Controllers
         }
         public ActionResult Index()
         {
+            //admin panalinde gerekl olan ürünler listelendi
             var ProductListViewModel = new ProductListViewModel
             {
                 Products = _productService.GetAll()
@@ -26,6 +28,20 @@ namespace MTS.Northwind.AspMvcCoreUI.Controllers
             return View(ProductListViewModel);
 
         
+        }
+        [HttpPost]
+        public ActionResult Add(Product product)
+        {
+            _productService.Add(product);
+            TempData.Add("message", "Product was add a successfully");
+            return View();
+
+        }
+        public ActionResult Add()
+        {
+            return View();
+
+
         }
     }
 }
---
+++ MTS/MTS.Northwind.AspMvcCoreUI/Models/ProductAddViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MTS.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTS.Northwind.AspMvcCoreUI.Models
+{
+    public class ProductAddViewModel
+    {
+        public Product Product { get; set; }
+    }
+}
