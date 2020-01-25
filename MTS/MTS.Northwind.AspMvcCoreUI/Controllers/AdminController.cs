b7aa57c1e4c1b93a2d3da29910bce33efef808b4
mtsoydan <mustafataha.soydan@gmail.com>
2020-01-26 02:51:00 +0300

    Identity ve Authentication.cookies paketleri eklendi

--- MTS/MTS.Northwind.AspMvcCoreUI/Controllers/AdminController.cs
+++ MTS/MTS.Northwind.AspMvcCoreUI/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MTS.Northwind.AspMvcCoreUI.Models;
 using MTS.Northwind.Business.Abstract;
@@ -9,6 +10,7 @@ using MTS.Northwind.Entities.Concrete;
 
 namespace MTS.Northwind.AspMvcCoreUI.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
 
